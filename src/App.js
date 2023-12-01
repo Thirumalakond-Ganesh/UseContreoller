a16aa2864c4f3fc7e8c7d47a99226b490c7b12c1
Thirumalakonda-Ganesh <ganitthirumala50@gmail.com>
2023-12-01 11:49:43 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
+import React from "react";
+import { useForm,useController } from "react-hook-form";
 import './App.css';
+function Input(props) {
+  const { field, fieldState } = useController(props);
 
-function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <input {...field} placeholder={props.name} />
+      <p>{fieldState.isTouched && "Touched"}</p>
+      <p>{fieldState.isDirty && "Dirty"}</p>
+      <p>{fieldState.invalid ? "invalid" : "valid"}</p>
     </div>
   );
 }
 
-export default App;
+export default function App() {
+  const { handleSubmit, control } = useForm({
+    defaultValues: {
+      FirstName: "",
+      LastName:""
+    },
+    mode: "onChange"
+  });
+  const onSubmit = (data) => console.log(data);
+
+  return (
+    <div>
+      <h1>useController</h1>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <Input control={control} name="FirstName" rules={{ required: true }} />
+        <Input control={control} name="LastName" rules={{ required: true }} />
+        <input type="submit" />
+      </form>
+    </div>
+  );
+}
